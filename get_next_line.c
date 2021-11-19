a610bf35d07f420f55b329874050c4cb56cbc5e2
Javier <javi@grey.local>
2021-11-20 01:59:12 +0200

    add eval_tests/main.c.test; wire up the whole thing and test it works

--- get_next_line.c
+++ get_next_line.c
@@ -3,18 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rodrodri <rodrodri@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: rodrodri <rodrodri@student.hive.fi >       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/19 23:00:34 by rodrodri          #+#    #+#             */
-/*   Updated: 2021/11/19 23:09:09 by rodrodri         ###   ########.fr       */
+/*   Updated: 2021/11/20 01:55:33 by rodrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include "libft.h"
 
 int get_next_line(const int fd, char **line)
 {
 	(void)fd;
 	(void)line;
+	ft_putstr("Hello world\n");
 	return (0);
 }
