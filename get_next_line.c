18b5eb4eafeac386efe85d602be8ecd793b95a98
Javier <javi@grey.local>
2021-11-29 21:13:04 +0200

    refactor get_next_line.c: rename function to 'read_ln'; move all read operations there.

--- get_next_line.c
+++ get_next_line.c
@@ -6,32 +6,31 @@
 /*   By: rodrodri <rodrodri@student.hive.fi >       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/19 23:00:34 by rodrodri          #+#    #+#             */
-/*   Updated: 2021/11/29 12:19:24 by rodrodri         ###   ########.fr       */
+/*   Updated: 2021/11/29 21:12:04 by rodrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static int	build_lst(int fd, char *buf, t_list **ln_lst, size_t *lst_len);
+static int	read_ln(int fd, char *buf, t_list **ln_lst, size_t *lst_len);
 static int	build_ln(char **ln, char *buf, t_list **ln_lst, size_t lst_len);
 static void	free_willy(void *content, size_t content_size);
 
 int	get_next_line(const int fd, char **ln)
 {
 	static char	buf[BUFF_SIZE + 1] = {0};
-	ssize_t		bytes_read;
 	t_list		*ln_lst;
 	size_t		lst_len;
+	ssize_t		ret;
 
 	ln_lst = NULL;
 	lst_len = 0;
 	if (fd < 0 || fd == 1 || fd == 2 || !ln)
 		return (-1);
-	bytes_read = read(fd, buf + ft_strlen(buf), BUFF_SIZE - ft_strlen(buf));
-	if (bytes_read < 0 || (bytes_read == 0 && ft_strlen(buf) == 0))
-		return (bytes_read);
-	if ((build_lst(fd, buf, &ln_lst, &lst_len) < 0) || \
-		(build_ln(ln, buf, &ln_lst, lst_len) < 0))
+	ret = read_ln(fd, buf, &ln_lst, &lst_len);
+	if (ret == 0)
+		return (0);
+	if (ret < 0 || build_ln(ln, buf, &ln_lst, lst_len) < 0)
 	{
 		if (ln_lst)
 			ft_lstdel(&ln_lst, free_willy);
@@ -40,12 +39,15 @@ int	get_next_line(const int fd, char **ln)
 	return (1);
 }
 
-static int	build_lst(int fd, char *buf, t_list **ln_lst, size_t *lst_len)
+static int	read_ln(int fd, char *buf, t_list **ln_lst, size_t *lst_len)
 {
 	t_list	*ln_node;
-	ssize_t	bytes_read;
+	ssize_t	ret;
 
-	while (ft_strlen(buf) == BUFF_SIZE && !ft_strchr(buf, '\n'))
+	ret = read(fd, buf + ft_strlen(buf), BUFF_SIZE - ft_strlen(buf));
+	if (ret == 0 && ft_strlen(buf) == 0)
+		return (0);
+	while (ft_strlen(buf) == BUFF_SIZE && !ft_strchr(buf, '\n') && ret > 0)
 	{
 		*lst_len += BUFF_SIZE;
 		ln_node = ft_lstnew(buf, BUFF_SIZE + 1);
@@ -53,11 +55,11 @@ static int	build_lst(int fd, char *buf, t_list **ln_lst, size_t *lst_len)
 			return (-1);
 		ft_lst_push_back(ln_lst, ln_node);
 		ft_strclr(buf);
-		bytes_read = read(fd, buf, BUFF_SIZE);
-		if (bytes_read < 0)
-			return (-1);
+		ret = read(fd, buf, BUFF_SIZE);
 	}
-	return (bytes_read);
+	if (ret < 0)
+		return (-1);
+	return (1);
 }
 
 static int	build_ln(char **ln, char *buf, t_list **ln_lst, size_t lst_len)
